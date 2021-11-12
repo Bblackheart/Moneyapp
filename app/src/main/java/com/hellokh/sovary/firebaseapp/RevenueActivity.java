4e4cf59a2df6d6bf9261dcc7abe58144e0bee90f
GT <zazatar99@gmail.com>
2021-11-12 13:17:37 +0700

    เพิ่มcode back

--- app/src/main/java/com/hellokh/sovary/firebaseapp/RevenueActivity.java
+++ app/src/main/java/com/hellokh/sovary/firebaseapp/RevenueActivity.java
@@ -2,7 +2,10 @@ package com.hellokh.sovary.firebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RevenueActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class RevenueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_revenue);
+
+        Button buttonBack = findViewById(R.id.Back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RevenueActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
