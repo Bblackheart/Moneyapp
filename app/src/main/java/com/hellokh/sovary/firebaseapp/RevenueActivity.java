4d528e95796b798a518099f212d3aeeeed45672b
project <mrzigar13@gmail.com>
2021-11-25 09:15:53 +0700

    อัพเดต 25/11/64

--- app/src/main/java/com/hellokh/sovary/firebaseapp/RevenueActivity.java
+++ app/src/main/java/com/hellokh/sovary/firebaseapp/RevenueActivity.java
@@ -18,18 +18,29 @@ public class RevenueActivity extends AppCompatActivity {
     private DatabaseReference reff;
     ReadWriteUserDetails readWriteUserDetails;
     private Button clearbtn;
+    private EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_revenue);
 
+        display = findViewById(R.id.edittext_number_1);
         editTextNumber = findViewById(R.id.edittext_number_1);
+        editTextNumber.setShowSoftInputOnFocus(false);
         submitbtn = findViewById(R.id.Submit_btn);
         reff = FirebaseDatabase.getInstance().getReference().child("Registered Users");
         getSupportActionBar().setTitle("Revenue");
         readWriteUserDetails = new ReadWriteUserDetails();
         clearbtn = findViewById(R.id.clear_btn);
+
+        editTextNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getString(R.string.display).equals(display.getText().toString()))
+                    display.setText("");
+            }
+        });
 
         Button buttonBack = findViewById(R.id.Back);
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -57,5 +68,42 @@ public class RevenueActivity extends AppCompatActivity {
                 editTextNumber.getText().clear();
             }
         });
+    }
+    private void updateText(String strToAdd){
+        String oldStr = display.getText().toString();
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+        if (getString(R.string.display).equals(display.getText().toString())) {
+            display.setText(strToAdd);
+            display.setSelection(cursorPos + 1);
+        } else {
+            display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
+            display.setSelection(cursorPos + 1);
+        }
+    }
+
+    public void onebtn(View view){
+        updateText("1");
+    }
+
+    public void fivebtn(View view){
+        updateText("5");
+    }
+
+    public void tenbtn(View view){
+        updateText("10");
+    }
+
+    public void twentybtn(View view){
+        updateText("20");
+    }
+
+    public void fiftybtn(View view){
+        updateText("50");
+    }
+
+    public void onehundredbtn(View view){
+        updateText("100");
     }
 }
